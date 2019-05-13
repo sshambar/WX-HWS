b9099fddd6a1945827340ccbfc3b77f4ac190ed7
Ian Millard <steepleian@gmail.com>
2019-05-13 17:53:34 +0100

    Update updater.php

--- updater.php
+++ updater.php
@@ -1,10 +1,24 @@
-<?php include('settings1.php');?>
+<!-- begin updater.php  30-Mar-2019 -->
+<?php
+include_once('settings1.php');
+include_once('common.php');
+date_default_timezone_set($TZ);
+?>
 <script src="js/jquery.js"></script>
-<script Defer>
+<script>
+ //update the charts,eq,forecast data and current conditions//
+  var refreshId;$(document).ready(function(){stationcron()});function stationcron(){$.ajax({cache:false,
+  success:function(a){$("#blank")
+  .html(a);<?php if ($wuupdate >0) {
+  echo 'setTimeout(stationcron,' . 221000*$wuupdate.')';}?>},
+  contentType: "application/x-www-form-urlencoded;charset=ISO-8859-15",
+  type:"GET",url:"jsondata/wuupdate.php"})}; 
+
 //update the modules
+
 //update the modules position 1
 var refreshId;$(document).ready(function(){position1()});function position1(){$.ajax({cache:false,success:function(a){$("#position1").html(a);<?php if ($notifyRefresh > 0) {
-	echo 'setTimeout(position1,' . 160000*$indoorRefresh.')';
+	echo 'setTimeout(position1,' . 221000*$indoorRefresh.')';
 } ?>},
   contentType: "application/x-www-form-urlencoded;charset=ISO-8859-15",
   type:"GET",url:"<?php echo $position1 ;?>"})};  
@@ -30,27 +44,33 @@ var refreshId;$(document).ready(function(){notification()});function notificatio
 // outdoor temp
 var refreshId;$(document).ready(function(){temperature()});function temperature(){$.ajax({cache:false,success:function(a){$("#temperature").html(a);<?php if ($tempRefresh > 0) {
     echo 'setTimeout(temperature,' . 1000*$tempRefresh.')';
-} ?>},type:"GET",url:"<?php echo $temperaturemodule;?>"})};
+} ?>},type:"GET",url:"<?php echo $temperaturemodule?>"})};
+
 //current conditions icon
 var refreshId;$(document).ready(function(){currentsky()});function currentsky(){$.ajax({cache:false,success:function(a){$("#currentsky").html(a);<?php if ($skyRefresh > 0) {
     echo 'setTimeout(currentsky,' . 1000*$skyRefresh.')';
 } ?>},type:"GET",url:"<?php echo $currentconditions ;?>"})};
-/// wind speed / direction 
+
+// wind speed / direction 
 var refreshId;$(document).ready(function(){windspeed()});function windspeed(){$.ajax({cache:false,success:function(a){$("#windspeed").html(a);<?php if ($windSpeedRefresh > 0) {
     echo 'setTimeout(windspeed,' . 1000*$windSpeedRefresh.')';
 } ?>},type:"GET",url:"windspeeddirection.php"})};     
+
 //barometer
 var refreshId;$(document).ready(function(){barometer()});function barometer(){$.ajax({cache:false,success:function(a){$("#barometer").html(a);<?php if ($baroRefresh > 0) {
     echo 'setTimeout(barometer,' . 1000*$baroRefresh.')';
 } ?>},type:"GET",url:"barometer.php"})};
+
 // moonphase
 var refreshId;$(document).ready(function(){moonphase()});function moonphase(){$.ajax({cache:false,success:function(a){$("#moonphase").html(a);<?php if ($moonRefresh > 0) {
     echo 'setTimeout(moonphase,' . 1000*$moonRefresh .')';
 } ?>},type:"GET",url:"<?php echo $sunoption?>"})};
+
 // rainfall
 var refreshId;$(document).ready(function(){rainfall()});function rainfall(){$.ajax({cache:false,success:function(a){$("#rainfall").html(a);<?php if ($rainRefresh > 0) {
     echo 'setTimeout(rainfall,' . 1000*$rainRefresh.')';
 } ?>},type:"GET",url:"rainfall.php"})};
+
 // position12
 var refreshId;$(document).ready(function(){solar()});function solar(){$.ajax({cache:false,success:function(a){$("#solar").html(a);<?php if ($solarRefresh > 0) {
     echo 'setTimeout(solar,' . 1000*$solarRefresh.')';
@@ -58,55 +78,92 @@ var refreshId;$(document).ready(function(){solar()});function solar(){$.ajax({ca
 
 //last module
 var refreshId;$(document).ready(function(){dldata()});function dldata(){$.ajax({cache:false,success:function(a){$("#dldata").html(a);<?php if ($daylightRefresh > 0) { echo 'setTimeout(dldata,' . 1000*$daylightRefresh.')';  } ?>}, type:"GET",url:"<?php echo $positionlastmodule?>"})}; 
+
 //current 3dy forecast
-var refreshId;$(document).ready(function(){currentfore()});function currentfore(){$.ajax({cache:false,success:function(a){$("#currentfore").html(a);setTimeout(currentfore,360000)},type:"GET",url:"<?php echo $position6?>"})};</script>
+var refreshId;$(document).ready(function(){currentfore()});function currentfore(){$.ajax({cache:false,success:function(a){$("#currentfore").html(a);setTimeout(currentfore,360000)},type:"GET",url:"<?php echo $position6?>"})};
+
+</script>
 <?php if ($position1=="weather34clock.php"){?>
 <script>
-var clockID;
-var yourTimeZoneFrom=<?php echo $UTC?>;
-var d=new Date();
-var weekdays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var months=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-var tzDifference=yourTimeZoneFrom*60+d.getTimezoneOffset();
-var offset=tzDifference*60*1000;
-function UpdateClock(){
-  var e=new Date(new Date().getTime()+offset);
-  var c=e.getHours()<?php if ($clockformat == '12') { echo '% 12 || 12';} else { echo '% 24 || 24';}?>;
-  <?php
-  if($clockformat=='12') {
-    echo "if(e.getHours()<12){amorpm=' am'}else{amorpm=' pm'}";
-  } else {
-    echo "amorpm='';";
-  }
-  ?>
-  var a=e.getMinutes();
-  var g=e.getSeconds();
-  var f=e.getFullYear();
-  var h=months[e.getMonth()];
-  var b=e.getDate();
-  var i=weekdays[e.getDay()];
-  if(a<10){
-    a="0"+a
-  }
-  if(g<10){
-    g="0"+g
-  }
-  if(c<10){
-    c="0"+c
-  }
-  document.getElementById("theTime").innerHTML="<div class='weatherclock34'> "+i+" "+b+" "+h+" "+f+"<div class='orangeclock'>"+c+":"+a+":"+g+amorpm
-}
-function StartClock(){clockID=setInterval(UpdateClock,500)}
-function KillClock(){clearTimeout(clockID)}
-window.onload=function(){StartClock()};
-</script><?php }?>
+// power the clock display in position1 using JavaScript
+var clockID;var yourTimeZoneFrom=<?php echo $UTC?>;var d=new Date();
+<?php 
+// Added code to display clock using supported languages - ktrue - 30-Mar-2019
+$rc = setlocale(LC_TIME,explode(',',$lang_locale));
+print "// language='$language' lang_locale='$lang_locale' setlocale='$rc' \n";
 
-<script DEFER type="text/livescript">
- //update the charts,eq,forecast data and current conditions//
-  var refreshId;$(document).ready(function(){stationcron()});function stationcron(){$.ajax({cache:false,
-  success:function(a){$("#blank")
-  .html(a);<?php if ($wuupdate >0) {
-  echo 'setTimeout(stationcron,' . 1000*$wuupdate.')';}?>},
-  contentType: "application/x-www-form-urlencoded;charset=ISO-8859-15",
-  type:"GET",url:"<?php if ($chartsource =='chartswu'){echo 'jsondata/wuupdate.php';}?>"})}; 
-  </script>   
+$months = array_map(
+    function($m){
+        return strftime("%b",mktime(0, 0, 0, $m, 2, 1970)) ;
+    },
+    range(1, 12)
+);
+//var months=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+print "var months=[";
+foreach ($months as $n => $abbr) {
+	if($n>0) {print ",";}
+	print "\"$abbr\"";
+}
+print "]; //using $language month names\n";
+
+$days = array_map(
+    function($m) {
+			  return strftime("%a",mktime(0, 0, 0, 1, $m+3, 1970)) ; // days of the week (Sun first)
+    },
+    range(1, 7)
+);
+
+//var weekdays=["Sun","Mon","Tue","Wed","Thur","Fri","Sat"];
+print "var weekdays=[";
+foreach ($days as $n => $abbr) {
+	if($n>0) {print ",";}
+	print "\"$abbr\"";
+}
+print "]; //using $language day names\n";
+print "var useAMPM = ";
+print preg_match('|g|',$timeFormat)?' true':' false';
+print " // time format\n";
+print "var ampmLegend = '".date('a')."';\n";
+?>
+var tzDifference = yourTimeZoneFrom * 60 + d.getTimezoneOffset();
+var offset = tzDifference * 60 * 1000;
+function UpdateClock() {
+	var e = new Date(new Date().getTime() + offset);
+	var c = e.getHours();
+	var a = e.getMinutes();
+	var g = e.getSeconds();
+	var f = e.getFullYear();
+	var h = months[e.getMonth()];
+	var b = e.getDate();
+	var i = weekdays[e.getDay()];
+	if (a < 10) {
+		a = "0" + a
+	}
+	if (g < 10) {
+		g = "0" + g
+	}
+	if (!useAMPM & c < 10) {
+		c = "0" + c
+	}
+	var c2 = c;
+	if (useAMPM) {
+	  if (c > 12)  {c2 = c - 12; ampm = 'pm'} else { ampm = 'am' } // afternoon v.s. morning
+		if (c == 12) { ampm='pm' } // noon
+	  if (c < 1 )  {c2 = c+12;} // midnight
+	}
+	
+	if(useAMPM) {c = c2;} else {ampm = '';}
+	document.getElementById("theTime").innerHTML = "<div class='weatherclock34'> " + i + " " + b + " " + h + " " + f + "<div class='orangeclock'>" + c + ":" + a + ":" + g + ampm
+}
+function StartClock() {
+	clockID = setInterval(UpdateClock, 500)
+}
+function KillClock() {
+	clearTimeout(clockID)
+}
+window.onload = function () {
+	StartClock()
+};
+</script>
+<?php }?>
+<!-- end updater.php -->

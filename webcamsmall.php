443114a04008a05f8867d832f7002b75802b9178
Ian Millard <steepleian@gmail.com>
2019-05-13 07:41:52 +0100

    Update webcamsmall.php

--- webcamsmall.php
+++ webcamsmall.php
@@ -14,5 +14,5 @@
 ?>
 </span></div>
 <!-- HOMEWEATHER STATION TEMPLATE SIMPLE WEBCAM -add your url as shown below do NOT delete the class='webcam' !!! -->
-<img src="<?php echo $webcamurl?>?v=<?php echo date('YmdGis');?>>" alt="weathercam" class="webcam">
+<img src="<?php echo $webcamurl?>?v=<?php echo date('YmdGis');?>" alt="weathercam" class="webcam">
 </span>

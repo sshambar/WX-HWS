a5a48dec2ba9d9f85fdacbee89430d579fc6bf86
Ian Millard <steepleian@gmail.com>
2019-05-22 09:25:02 +0100

    Update yearlywindspeedgustmedium.php

--- mbcharts/yearlywindspeedgustmedium.php
+++ mbcharts/yearlywindspeedgustmedium.php
@@ -1,37 +1,3 @@
-<?php
-
-	####################################################################################################
-	#	WUDATACHARTS by BRIAN UNDERDOWN 2016                                                           #
-	#	CREATED FOR HOMEWEATHERSTATION TEMPLATE at http://weather34.com/homeweatherstation/index.html  #
-	# 	                                                                                               #
-	# 	built on CanvasJs  	                                                                           #
-	#   canvasJs.js is protected by CREATIVE COMMONS LICENCE BY-NC 3.0  	                           #
-	# 	free for non commercial use and credit must be left in tact . 	                               #
-	# 	                                                                                               #
-	# 	Weather Data is based on your PWS upload quality collected at Weather Underground 	           #
-	# 	                                                                                               #
-	# 	Second General Release: 4th October 2016  	                                                   #
-	# 	                                                                                               #
-	#   http://www.weather34.com 	                                                                   #
-	####################################################################################################
-
-	include('chartslivedata.php');include('./chart_theme.php');header('Content-type: text/html; charset=utf-8');
-	$weatherfile = date('Y');
-
-  $conv = 1;
-
-	if ($windunit == 'mph') {
-    $conv= '0.621371';
-  } else if ($windunit == 'm/s') {
-    $conv= '0.277778';
-  } else if ($windunit == 'km/h'){
-    $conv= '1';
-  }
-
-	//echo $conv;
-
-	$animationduration = '500';
-
 ?>
 
 <!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -50,68 +16,64 @@
 		var dataPoints2 = [];
 		$.ajax({
 			type: "GET",
-			url: "chartdata/<?php echo $weatherfile;?>.csv",
+			url: "<?php echo $weatherfile;?>.csv",
 			dataType: "text",
 			cache:false,
 			success: function(data) {processData1(data),processData2(data);}
 		});
-
 	function processData1(allText) {
 		var allLinesArray = allText.split('\n');
 		if(allLinesArray.length>0){
-
 			for (var i = 0; i <= allLinesArray.length-1; i++) {
 				var rowData = allLinesArray[i].replace(/�|\"/g,'').split(',');
 				if ( rowData.length >1)
 					dataPoints1.push({label: rowData[0],y:parseFloat(rowData[6]*<?php echo $conv;?>)});
-
-
 			}
 		}
 		requestTempCsv();}function requestTempCsv(){}
-
 	function processData2(allText) {
 		var allLinesArray = allText.split('\n');
 		if(allLinesArray.length>0){
-
 			for (var i = 0; i <= allLinesArray.length-1; i++) {
 				var rowData = allLinesArray[i].replace(/�|\"/g,'').split(',');
 				if ( rowData.length >1)
 					dataPoints2.push({label: rowData[0],y:parseFloat(rowData[7]*<?php echo $conv;?>)});
 					//parseFloat(rowData[13])});
-
 			}
 			drawChart(dataPoints1 , dataPoints2 );
 		}
 	}
-
-
 	function drawChart( dataPoints1 , dataPoints2 ) {
 		var chart = new CanvasJS.Chart("chartContainer2", {
-		backgroundColor: "rgba(40, 45, 52,.4)",
-		 animationEnabled: false,
+		backgroundColor: '<?php echo $darkbackgroundcolor;?>',
+		animationEnabled: true,
+		animationDuration: <?php echo $animationduration;?>,
 		 margin: 0,
-
-
 		title: {
             text: " ",
 			fontSize: 11,
-			fontColor: '#aaa',
+			fontColor: '<?php echo $darkfontcolor;?>',
 			fontFamily: "arial",
         },
 		toolTip:{
 			   fontStyle: "normal",
 			   cornerRadius: 4,
-			   backgroundColor: "rgba(40, 45, 52,1)",	
-			   fontColor: '#aaa',	
+			   backgroundColor: '<?php echo $tooltipbackgroundcolor;?>',
 			   fontSize: 11,	   
-			   toolTipContent: " x: {x} y: {y} <br/> name: {name}, label:{label} ",
+			   contentFormatter: function(e) {
+      var str = '<span style="color: <?php echo $darkfontcolor;?>;">' + CanvasJS.formatDate(e.entries[0].dataPoint.label, "DD MMM") + '</span><br/>';
+      for (var i = 0; i < e.entries.length; i++) {
+        var temp = '<span style="color: ' + e.entries[i].dataSeries.color + ';">' + e.entries[i].dataSeries.name + '</span> <span style="color: <?php echo $darkfontcolor;?>;">' + e.entries[i].dataPoint.y.toFixed(1) + "<?php echo ' '.$windunit ;?>" + '</span> <br/>';
+        str = str.concat(temp);
+      }
+      return (str);
+    },
 			   shared: true, 
  },
 		axisX: {
-			gridColor: '#333',
+			gridColor: '<?php echo $darkgridcolor;?>',
 		    labelFontSize: 10,
-			labelFontColor: '#aaa',
+			labelFontColor: '<?php echo $darkfontcolor;?>',
 			lineThickness: 1,
 			gridThickness: 1,
 			titleFontFamily: "arial",
@@ -123,16 +85,13 @@
 			crosshair: {
 			enabled: true,
 			snapToDataPoint: true,
-			color: '<?php echo $xcrosshaircolor;?>',
+			color: '<?php echo $darkxcrosshaircolor;?>',
 			labelFontColor: "#F8F8F8",		
-			
 			labelFontSize:8,
 			labelMaxWidth: 60,
-			labelBackgroundColor: "#44a6b5",
+			labelBackgroundColor: '<?php echo $darkxcrosshaircolor;?>',
 		}
-
 			},
-
 		axisY:{
 		title: "",
 		titleFontColor: "#555",
@@ -142,9 +101,9 @@
 		lineThickness: 1,
 		gridThickness: 1,
         includeZero: false,
-		gridColor: '#333',
+		gridColor: '<?php echo $darkgridcolor;?>',
 		labelFontSize: 11,
-		labelFontColor: '#aaa',
+		labelFontColor: '<?php echo $darkfontcolor;?>',
 		gridDashType: "dot",
 		titleFontFamily: "arial",
 		labelFontFamily: "arial",
@@ -154,59 +113,53 @@
 		 crosshair: {
 			enabled: true,
 			snapToDataPoint: true,
-			color: '<?php echo $ycrosshaircolor;?>',
+			color: '<?php echo $darkycrosshaircolor;?>',
 			labelFontColor: "#F8F8F8",
-			labelFontSize:10,
+			labelFontSize:11,
 			labelMaxWidth: 60,
-			labelBackgroundColor: "#44a6b5",
-			valueFormatString: "#0.#<?php echo $windunit ;?>",
+			labelBackgroundColor: '<?php echo $darkycrosshaircolor;?>',
+			valueFormatString: "#0.0<?php echo $windunit ;?>",
 		}
-
       },
-
 	  legend:{
       fontFamily: "arial",
-      fontColor: '#aaa',
-
+      fontColor: '<?php echo $darkfontcolor;?>',
  },
-
 		data: [
 		{
 			// Max Wind Gust
-			type: "column",
-			color: '#d35d4e',
+			type: "splineArea",
+			color: '<?php echo $darkline1color;?>',
+			lineColor: '<?php echo $darkline1linecolor;?>',
 			markerSize:0,
-			showInLegend:false,
-			legendMarkerType: "circle",
-			lineThickness: 0,
-			markerType: "circle",
-			name:"Max Wind Gust",
-			dataPoints: dataPoints1,
-			yValueFormatString:"#0.# <?php echo $windunit ;?>",
-		},
-		{
-			// Average Wind Speed
-			type: "spline",
-			color: 'rgba(0, 164, 180, 1.000)',
-			markerSize:0,
-     		markerColor: 'rgba(0, 164, 180, 1.000)',
 			showInLegend:false,
 			legendMarkerType: "circle",
 			lineThickness: 2,
-     		 lineColor: 'rgba(0, 164, 180, 1.000)',
+			markerType: "circle",
+			name:"Max Wind Gust",
+			dataPoints: dataPoints1,
+			yValueFormatString:"#0.0 <?php echo $windunit ;?>",
+		},
+		{
+			// Average Wind Speed
+			type: "splineArea",
+			color: '<?php echo $darkline2color;?>',
+			markerSize:0,
+      		markerColor: '<?php echo $darkline2markercolor;?>',
+			showInLegend:false,
+			legendMarkerType: "circle",
+			lineThickness: 2,
+      		lineColor: '<?php echo $darkline2markercolor;?>',
 			markerType: "circle",
 			name:"Avg Wind Speed",
 			dataPoints: dataPoints2,
-			yValueFormatString:"#0.# <?php echo $windunit ;?>",
+			yValueFormatString:"#0.0 <?php echo $windunit ;?>",
 		}
-
 		]
 		});
-
 		chart.render();
 	}
 });
-
    </script>
 
 <body>

e4e53477a01b2075e4ddf35251ec4d0ed98c1876
Ian Millard <steepleian@gmail.com>
2019-05-23 22:31:35 +0100

    Update monthlybarometer.php

--- mbcharts/monthlybarometer.php
+++ mbcharts/monthlybarometer.php
@@ -62,7 +62,7 @@
 		var dataPoints2 = [];
 		$.ajax({
 			type: "GET",
-			url: "chartdata/<?php echo date('Y');?>/<?php echo $weatherfile;?>.csv",
+			url: "chartdata/<?php echo $weatherfile;?>.csv",
 			dataType: "text",
 			cache:false,
 			success: function(data) {processData1(data),processData2(data);}
